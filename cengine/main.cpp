ec1d2ef7e94aba5b84b2631a52aac9a748fcc9cf
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-10-17 22:16:33 +0330

    commit

--- cengine/main.cpp
+++ cengine/main.cpp
@@ -9,14 +9,25 @@ int main(int argc, char* argv[])
 {
 	try
 	{
-		const SDL::Init sdl_init; if (sdl_init.is_not_valid) return EXIT_FAILURE;
-		const SDL::GL sdl_gl{ 2, 1 }; if (sdl_gl.is_not_valid) return EXIT_FAILURE;
-		const SDL::Window window; if (window.is_not_valid) return EXIT_FAILURE;
-		const SDL::Context context{ window }; if (context.is_not_valid) return EXIT_FAILURE;
+		const SDL::Init sdl_init;             if (sdl_init.is_not_valid) { return EXIT_FAILURE; }
+		const SDL::GL sdl_gl{ 2, 1 };         if (sdl_gl.is_not_valid) { return EXIT_FAILURE; }
+		const SDL::Window window;             if (window.is_not_valid) { return EXIT_FAILURE; }
+		const SDL::Context context{ window }; if (context.is_not_valid) { return EXIT_FAILURE; }
 		
+		gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+		printf("Vendor:   %s\n", glGetString(GL_VENDOR));
+		printf("Renderer: %s\n", glGetString(GL_RENDERER));
+		printf("Version:  %s\n", glGetString(GL_VERSION));
 
-		SDL_FillRect(window.surface, nullptr, SDL_MapRGB(window.surface->format, 0xFF, 0xFF, 0xFF));
-		SDL_UpdateWindowSurface(window.window);
+		std::cout << "SDL_GL_SetSwapInterval: " << SDL_GL_SetSwapInterval(1) << std::endl;
+
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_CULL_FACE);
+
+		int w, h;
+		SDL_GetWindowSize(window.window, &w, &h);
+		glViewport(0, 0, w, h);
+		glClearColor(0.0f, 0.5f, 1.0f, 0.0f);
 
 		bool is_running{ true };
 		while (is_running)
@@ -29,6 +40,7 @@ int main(int argc, char* argv[])
 					is_running = false;
 				}
 			}
+			SDL_GL_SwapWindow(window.window);
 		}
 	}
 	catch (const std::exception& e)
